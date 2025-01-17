46bde7370c498189e9e0727067c1db14675f9993
natsun08 <amuchan134@gmail.com>
2025-01-18 01:43:15 +0700

    Add dash

--- Assets/Scripts/Core/Movement/PlayerMovement.cs
+++ Assets/Scripts/Core/Movement/PlayerMovement.cs
@@ -7,17 +7,24 @@ public class PlayerMovement : MonoBehaviour
     //Declare variables
 
     public float moveSpeed;
+    public float dashSpeed;
+    public float dashCooldown;
+    public float dashDuration;
     Rigidbody2D rb;
+    bool isDashing = false;
+    bool canDash = true;
     public float lastHorizontalVector;
     public float lastVerticalVector;
-
     public Vector2 moveDir;
+
+
 
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        canDash = true;
     }
 
     // Update is called once per frame
@@ -30,6 +37,9 @@ public class PlayerMovement : MonoBehaviour
     }
 
     void InputManagement(){
+        if (isDashing){
+            return;
+        };
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY =  Input.GetAxisRaw("Vertical");
         moveDir = new Vector2(moveX, moveY).normalized;
@@ -40,12 +50,27 @@ public class PlayerMovement : MonoBehaviour
         if (moveDir.y !=0){
             lastVerticalVector = moveDir.y;
         }
+        if (Input.GetKeyDown(KeyCode.Space) && canDash){
+            StartCoroutine(Dash());
+        }
+
     }
 
     void Move(){
+        if (isDashing){
+            return;
+        };
         rb.velocity = new Vector2(moveDir.x * moveSpeed, moveDir.y * moveSpeed);
-        
     }
 
+    IEnumerator Dash(){
+        canDash = false;
+        isDashing = true;
+        rb.velocity = new Vector2(moveDir.x * dashSpeed, moveDir.y * dashSpeed);    
+        yield return new WaitForSeconds(dashDuration);
+        isDashing = false;
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
+    }
 
 }
