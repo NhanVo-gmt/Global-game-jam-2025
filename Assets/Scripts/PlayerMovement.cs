7cca4acc65b31820ecd089f2a8cb677d61a293ff
natsun08 <amuchan134@gmail.com>
2025-01-17 22:32:57 +0700

    setting up

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Declaration
+
+    public float moveSpeed;
+    Rigidbody2D rb;
+    Vector2 moveDir;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
