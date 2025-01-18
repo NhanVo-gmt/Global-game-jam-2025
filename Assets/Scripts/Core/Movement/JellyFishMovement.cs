1ed025ae9fe9e1d87b3452a9110023de82dcd7ea
NhanVoo <thanhnhan@osogames.studio>
2025-01-18 15:55:03 +0700

    update jelly fish movement

--- Assets/Scripts/Core/Movement/JellyFishMovement.cs
+++ Assets/Scripts/Core/Movement/JellyFishMovement.cs
@@ -1,18 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class JellyFishMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float force;
+    public float stopTime;
+    
+    private Transform      player;
+    private Rigidbody2D    rb;
+    private Coroutine      moveCoroutine;
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
     {
-        
+        rb             = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        player = FindObjectOfType<PlayerMovement>().transform;
+
+        moveCoroutine = StartCoroutine(MoveCoroutine());
+    }
+
+    IEnumerator MoveCoroutine()
+    {
+        float timeElapse = 0f;
+        while (true)
+        {
+            timeElapse -= Time.deltaTime;
+
+            if (timeElapse <= 0f)
+            {
+                timeElapse = stopTime;
+                if (player.transform.position.x < transform.position.x)
+                {
+                    spriteRenderer.flipX = true;
+                }
+                else spriteRenderer.flipX = false;
+
+                Vector2 direction = (player.transform.position - transform.position).normalized;
+                rb.AddForce(direction * force, ForceMode2D.Impulse);
+            }
+
+            yield return null;
+        }
     }
 }
