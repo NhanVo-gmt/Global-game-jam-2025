e44dd65d161bb7f4faeef2d2919164dec40def7c
NhanVoo <thanhnhan@osogames.studio>
2025-01-18 18:53:33 +0700

    add crab death

---
+++ Assets/Scripts/Game/CrabDeath.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+public class CrabDeath : EnemyDeath
+{
+    [Header("Damage")]
+    public float radius;
+    
+    protected override void Die()
+    {
+        base.Die();
+        
+    }
+
+    void DamageArea()
+    {
+        Collider2D[] cols = Physics2D.OverlapCircleAll(transform.position, radius);
+        foreach (var col in cols)
+        {
+            if (col.TryGetComponent<PlayerMovement>(out PlayerMovement playerMovement))
+            {
+                //todo damage player
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
