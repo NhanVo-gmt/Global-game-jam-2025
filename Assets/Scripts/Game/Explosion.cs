f26bb7dab42b99c25277cc9c5eca88fd40de0ee8
NhanVoo <thanhnhan@osogames.studio>
2025-01-19 02:04:04 +0700

    add crab explosion

---
+++ Assets/Scripts/Game/Explosion.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Explosion : AutoDestroyObject
+{
+    public Animator animator;
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        
+        animator.Rebind();
+        animator.Play("Idle");
+    }
+}
