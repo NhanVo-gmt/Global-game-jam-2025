30718957668a25dfc30243ae568da81767f7d229
NhanVoo <thanhnhan@osogames.studio>
2025-01-18 12:22:01 +0700

    flip sprite when moving

--- Assets/Scripts/Core/Movement/EnemyMovement.cs
+++ Assets/Scripts/Core/Movement/EnemyMovement.cs
@@ -1,11 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    Transform player;
-    public float moveSpeed;
+    public  float    moveSpeed;
+
+    private Transform player;
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +24,17 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, moveSpeed*Time.deltaTime);
+        Move();
+    }
+
+    void Move()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, moveSpeed * Time.deltaTime);
+
+        if (player.transform.position.x < transform.position.x)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else spriteRenderer.flipX = false;
     }
 }
